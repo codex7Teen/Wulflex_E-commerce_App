34dd256ce6160eb9433748a6bd2ef99a89274e27
Dennizx <djconnect189@gmail.com>
2024-10-28 18:26:17 +0530

    signup_screen

--- lib/screens/aunthentication_screens/signup_screen.dart
+++ lib/screens/aunthentication_screens/signup_screen.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wulflex/consts/app_colors.dart';
 import 'package:wulflex/consts/text_styles.dart';
-import 'package:wulflex/screens/aunthentication_screens/signup_screen.dart';
-import 'package:wulflex/widgets/google_button.dart';
+import 'package:wulflex/screens/aunthentication_screens/login_screen.dart';
 import 'package:wulflex/widgets/green_button.dart';
 
-class ScreenLogin extends StatelessWidget {
-  const ScreenLogin({super.key});
+class ScreenSignUp extends StatelessWidget {
+  const ScreenSignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +24,45 @@ class ScreenLogin extends StatelessWidget {
 
                   // image
                   Center(
-                    child: Image.asset('assets/login_illustric.png',
+                    child: Image.asset('assets/signup_illustric.png',
                         width: MediaQuery.sizeOf(context).width * 0.645),
                   ),
                   SizedBox(height: 40),
 
                   // heading
-                  Text('Login',
+                  Text('SIGN UP',
                       style: GoogleFonts.bebasNeue(
                               textStyle: AppTextStyles.headingLarge)
                           .copyWith(letterSpacing: 1)),
                   SizedBox(height: 14),
+
+                  // Name textfield
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6),
+                        child: Icon(
+                          Icons.person,
+                          color: AppColors.greyThemeColor,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                  hintText: 'Full name',
+                                  hintStyle: GoogleFonts.robotoCondensed(
+                                      textStyle: AppTextStyles.mediumText
+                                          .copyWith(
+                                              color: Colors.grey,
+                                              letterSpacing: 0.5)),
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: AppColors.greyThemeColor,
+                                          width: 0.4))))),
+                    ],
+                  ),
+                  SizedBox(height: 22),
 
                   // email textfield
                   Row(
@@ -63,9 +90,9 @@ class ScreenLogin extends StatelessWidget {
                                           width: 0.4))))),
                     ],
                   ),
-                  SizedBox(height: 30),
+                  SizedBox(height: 22),
 
-                  // password field
+                  // create password field
                   Row(
                     children: [
                       Padding(
@@ -83,7 +110,7 @@ class ScreenLogin extends StatelessWidget {
                                     Icons.visibility_off_sharp,
                                     color: AppColors.greyThemeColor,
                                   ),
-                                  hintText: 'Password',
+                                  hintText: 'Create password',
                                   hintStyle: GoogleFonts.robotoCondensed(
                                       textStyle: AppTextStyles.mediumText
                                           .copyWith(
@@ -97,48 +124,40 @@ class ScreenLogin extends StatelessWidget {
                   ),
                   SizedBox(height: 22),
 
-                  // forgot password
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Text('Forgot Password?',
-                        style: GoogleFonts.robotoCondensed(
-                          textStyle: AppTextStyles.mediumText.copyWith(
-                              color: AppColors.greenThemeColor,
-                              letterSpacing: 0.5,
-                              fontWeight: FontWeight.bold),
-                        )),
-                  ),
-                  SizedBox(height: 22),
-
-                  // Login Button
-                  GreenButtonWidget(buttonText: 'Login'),
-                  SizedBox(height: 22),
-
-                  // OR divider with lines
+                  // confirm password field
                   Row(
                     children: [
-                      Expanded(
-                          child: Divider(
-                              color: AppColors.greyThemeColor, thickness: 0.4)),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'OR',
-                          style: GoogleFonts.roboto(
-                              textStyle: AppTextStyles.smallText.copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColors.greyThemeColor)),
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Icon(
+                          Icons.lock_outline_rounded,
+                          color: AppColors.greyThemeColor,
                         ),
                       ),
+                      SizedBox(width: 10),
                       Expanded(
-                          child: Divider(
-                              color: AppColors.greyThemeColor, thickness: 0.4))
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                  suffixIcon: Icon(
+                                    Icons.visibility_off_sharp,
+                                    color: AppColors.greyThemeColor,
+                                  ),
+                                  hintText: 'Confirm password',
+                                  hintStyle: GoogleFonts.robotoCondensed(
+                                      textStyle: AppTextStyles.mediumText
+                                          .copyWith(
+                                              color: Colors.grey,
+                                              letterSpacing: 0.5)),
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: AppColors.greyThemeColor,
+                                          width: 0.4))))),
                     ],
                   ),
                   SizedBox(height: 22),
 
-                  // Google Button
-                  GoogleButtonWidget(),
+                  // Login Button
+                  GreenButtonWidget(buttonText: 'Submit'),
                   SizedBox(height: 22),
 
                   // New User. Sign-UP text
@@ -146,7 +165,7 @@ class ScreenLogin extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'New to Wulflex?',
+                        'Joined us before?',
                         style: GoogleFonts.robotoCondensed(
                                 textStyle: AppTextStyles.mediumText)
                             .copyWith(
@@ -159,7 +178,7 @@ class ScreenLogin extends StatelessWidget {
                             .pushReplacement(PageRouteBuilder(
                           pageBuilder:
                               (context, animation, secondaryAnimation) =>
-                                  ScreenSignUp(),
+                                  ScreenLogin(),
                           transitionDuration: Duration(milliseconds: 400),
                           transitionsBuilder:
                               (context, animation, secondaryAnimation, child) {
@@ -168,7 +187,7 @@ class ScreenLogin extends StatelessWidget {
                           },
                         )),
                         child: Text(
-                          'Sign up',
+                          'Login',
                           style: GoogleFonts.robotoCondensed(
                                   textStyle: AppTextStyles.mediumText)
                               .copyWith(
